ae91ece1b062cbaed9b6db26fa48157bd8b4d3c1
ForNeVeR <neverthness@gmail.com>
2012-04-21 19:30:04 +0700

    Enabled window loop.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
 object Main {
   def main(args : Array[String]) {
     val window = new Window()
+    window.show()
+    window.loop()
   }
 }
--- src/main/scala/Window.scala
+++ src/main/scala/Window.scala
@@ -1,12 +1,32 @@
 import org.lwjgl.opengl._
 
 class Window {
-  val windowWidth = 800
-  val windowHeight = 600
+  private val windowWidth = 800
+  private val windowHeight = 600
 
-  val displayMode = new DisplayMode(windowWidth, windowHeight)
-  Display.setDisplayMode(displayMode)
-  Display.create()
-  GL11.glClearColor(0, 0, 0, 0)
-  GL11.glViewport(0, 0, windowWidth, windowHeight)
+  private val fps = 60
+
+  private val displayMode = new DisplayMode(windowWidth, windowHeight)
+
+  def show() {
+    Display.setDisplayMode(displayMode)
+    Display.create()
+    GL11.glClearColor(0, 0, 0, 0)
+    GL11.glViewport(0, 0, windowWidth, windowHeight)
+
+  }
+
+  def loop() {
+    def clearDisplay() {
+      // TODO: clear display.
+    }
+
+    while (!Display.isCloseRequested) {
+      Display.sync(fps)
+      clearDisplay()
+      Display.update()
+    }
+
+    Display.destroy()
+  }
 }
