d854a667cb62bce550a5400cbbaced5e2d29f416
ForNeVeR <neverthness@gmail.com>
2012-04-22 15:00:30 +0700

    Update src/main/scala/Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,7 @@ object Main {
   def main(args : Array[String]) {
     val buffer = new CircularBuffer[Double](300)
     //val generator = new RandomGenerator(buffer)
-    val generator = new ComGenerator(buffer, "COM1")
+    val generator = new ComGenerator(buffer, "COM3", 115200)
     val window = new Window(buffer)
 
     generator.start()
